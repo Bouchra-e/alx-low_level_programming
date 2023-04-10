524d3c707b9a3b09fc7315623d2dd693f7f273e1
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-04-10 23:18:31 +0200

    Create 1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return
