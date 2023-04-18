86a00aba71d565e46c30d5c6e0f5c05dfbf42e6e
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-04-18 02:42:37 +0200

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,14 +1,4 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
-/**
-* main - check the code
-* Return: Always 0.
-*/
-int main(void)
-{
-int s;
-s = 98 + SIZE;
-printf("%d\n",s);
-return (0);
-}
+#ifndef PI_H
+#define PI_H
+#define PI 3.14159265359
+#endif
