7996dcd90383faea8702aa656079aba16a53bca4
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-04-18 02:20:04 +0200

    Create 1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+int S;
+S = 98 + SIZE;
+printf("%d\n",S);
+return (0);
+}
