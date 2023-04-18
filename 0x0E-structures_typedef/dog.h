0a3ee985398723ac93a1f4ae70ea66ad274a4c10
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-04-18 03:34:07 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,9 +4,9 @@
 /**
  * init_dog-initializes a variable of type struct dog 
  * @d: pointer to struct dog to initialize
- * @name: dog's name
- * @age: dog's age
- * @owner: dog owner's name
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owners to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
