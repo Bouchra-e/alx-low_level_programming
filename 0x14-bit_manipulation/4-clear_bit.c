b7646c3c03514611a18992503f882b5cc8338b82
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-05-04 21:31:06 +0200

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * @index: index of the bit to clear
  * Return: 1 for success, -1 for failure
  */
-int claer_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
